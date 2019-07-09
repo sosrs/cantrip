c9208ffbb7d4ea685a59996cf3aa75f627a28c66
sosrs <rshah.samir@gmail.com>
2019-07-08 17:34:26 -0700

    Started with the basic rolling function

---
+++ cantrip.py
@@ -0,0 +1,5 @@
+# This will be the actual function app to track combat and skill challenges in DnD
+import cantripfunctions as cf
+import random
+import tkinter
+
---
+++ cantripfunctions.py
@@ -0,0 +1,12 @@
+# These are the custom functions I will use in my cantrip app
+
+
+def roll(num: int, die: int, bonus: int = 0)->int:
+
+    # This function will make a standard XdY + Z roll and return it as an integer
+    # Both the number of dice and the number of sides must be positive nonzero integers
+    from random import randint
+    for i in range(num):
+        total += randint(1,die)
+    return total + bonus
+
