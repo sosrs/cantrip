a42fc25cc1f66eba117aa8db7d89f708a825f610
sosrs <rshah.samir@gmail.com>
2019-07-08 17:36:20 -0700

    Started with the basic rolling function

--- cantripfunctions.py
+++ cantripfunctions.py
@@ -6,6 +6,7 @@ def roll(num: int, die: int, bonus: int = 0)->int:
     # This function will make a standard XdY + Z roll and return it as an integer
     # Both the number of dice and the number of sides must be positive nonzero integers
     from random import randint
+    total = 0
     for i in range(num):
         total += randint(1,die)
     return total + bonus
